314d8e73a0d5d042942a5016bcf3d2ff4a409fc8
Bolli123 <bjossi34@gmail.com>
2022-03-24 21:01:31 +0000

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
+
+export default config;
